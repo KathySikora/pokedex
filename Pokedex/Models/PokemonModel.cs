2976c05abd148e7b0500261a94bc00b711ea01be
Katharina Sikora <katharina.s1511@gmail.com>
2024-12-11 15:45:46 +0100

    größenfilter und lazyloading fertig

--- Pokedex/Models/PokemonModel.cs
+++ Pokedex/Models/PokemonModel.cs
@@ -45,7 +45,12 @@ namespace Pokedex.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        public List<string> Types { get; set; } = new();
+        public List<string> Abilities { get; set; } = new();
+        public int Height { get; set; }
+        public int Weight { get; set; }
     }
+
 
     public class PokemonListResponse
     {
--- Pokedex/Services/PokedexService.cs
+++ Pokedex/Services/PokedexService.cs
@@ -94,6 +94,75 @@ namespace Pokedex.Services
             }).ToList();
         }
 
+        public async Task<List<Pokemon>> GetAllPokemonsWithDetailsAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<PokemonListResponse>("https://pokeapi.co/api/v2/pokemon?limit=2500&offset=0");
+            if (response?.Results == null) return new List<Pokemon>();
+
+            var pokemons = new List<Pokemon>();
+
+            foreach (var result in response.Results)
+            {
+                var id = ExtractIdFromUrl(result.Url);
+                var detailResponse = await _httpClient.GetFromJsonAsync<PokemonDetailResponse>($"https://pokeapi.co/api/v2/pokemon/{id}");
+
+                if (detailResponse != null)
+                {
+                    pokemons.Add(new Pokemon
+                    {
+                        Id = id,
+                        Name = detailResponse.Name,
+                        ImageUrl = $"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png",
+                        Types = detailResponse.Types.Select(t => t.Type.Name).ToList(),
+                        Height = detailResponse.Height,
+                        Weight = detailResponse.Weight
+                    });
+                }
+            }
+
+            return pokemons;
+        }
+
+        public async IAsyncEnumerable<List<Pokemon>> GetPokemonBatchesAsync(int batchSize = 100)
+        {
+            var response = await _httpClient.GetFromJsonAsync<PokemonListResponse>("https://pokeapi.co/api/v2/pokemon?limit=2500&offset=0");
+            if (response?.Results == null) yield break;
+
+            var pokemons = new List<Pokemon>();
+
+            foreach (var (pokemon, index) in response.Results.Select((p, i) => (p, i)))
+            {
+                var id = ExtractIdFromUrl(pokemon.Url);
+                var detailResponse = await _httpClient.GetFromJsonAsync<PokemonDetailResponse>($"https://pokeapi.co/api/v2/pokemon/{id}");
+
+                if (detailResponse != null)
+                {
+                    pokemons.Add(new Pokemon
+                    {
+                        Id = id,
+                        Name = detailResponse.Name,
+                        ImageUrl = $"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png",
+                        Types = detailResponse.Types.Select(t => t.Type.Name).ToList(),
+                        Abilities = detailResponse.Abilities.Select(a => a.Ability.Name).ToList(),
+                        Height = detailResponse.Height,
+                        Weight = detailResponse.Weight
+                    });
+                }
+
+                if ((index + 1) % batchSize == 0)
+                {
+                    yield return pokemons.ToList();
+                    pokemons.Clear();
+                }
+            }
+
+            if (pokemons.Any())
+            {
+                yield return pokemons;
+            }
+        }
+
+
 
         private int ExtractIdFromUrl(string url)
         {
